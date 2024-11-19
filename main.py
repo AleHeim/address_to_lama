792eea2c46ac037438811fbab23dd75441af5c2c
AleHeim <70062777+AleHeim@users.noreply.github.com>
2024-11-19 18:55:54 +0300

    comments

--- main.py
+++ main.py
@@ -2,31 +2,35 @@ import requests
 import json
 from translate import Translator
 
-headers = {
-    'Content-Type': 'application/json',
-	'charset': 'utf-8'
-}
+#headers = {
+#    'Content-Type': 'application/json',
+#	'charset': 'utf-8'
+#}
 
+# Формат '{ "model": "название_модели", "prompt":"запрос к модели"}'
 data = '{   "model": "llama3.2",   "prompt":"Who are you"  }'
 
 responses = []
 
-response = requests.post('http://172.17.0.2:11434/api/generate', data=data, )
+# Формат ('протокол://ip:port/путь', data=json данные) 
+response = requests.post('http://172.17.0.2:11434/api/generate', data=data )
 
 print(f'response STATUS CODE {response.status_code}')
 
+# Обработка ответа
 for line in response.iter_lines():
 	response_json = json.loads(line)
 	try:
 		responses.append(response_json["response"])
 	except:
 		pass
-
+		
 output = ''
 
 for word in responses:
 	output += word
 
+# Перевод и вывод
 translator = Translator(from_lang="en", to_lang="ru")
 output = translator.translate(output)
 print(output)
